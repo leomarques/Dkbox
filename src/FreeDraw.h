4c63d7ff178f1049158b37a2ab3f435bb0136877
leommarques7 <leommarques7@61357436-ba0b-11de-8784-f7ad4773dbab>
2010-01-29 23:10:10 +0000

    - Forgot to commit FreeDraw code.

---
+++ src/FreeDraw.h
@@ -0,0 +1,39 @@
+#ifndef FREEDRAW_H
+#define FREEDRAW_H
+
+#define INF 0x3f3f3f3f
+
+#include <Allegro.h>
+#include <vector>
+#include <algorithm>
+
+#include "MathUtils.h"
+#include "World.h"
+#include "Body.h"
+#include "RenderUtils.h"
+#include "GameUtils.h"
+
+class World;
+using namespace std;
+
+class FreeDraw
+{
+public:
+    BITMAP *bmp;
+    bool On;
+
+    FreeDraw(void);
+    ~FreeDraw(void);
+
+    void takePoint(Point p);
+    bool makeBody(World *world);
+
+private:
+    Point lastPoint, least, great;
+    vector<Point> points;
+
+    void addPoint(Point p);
+    void resetLG(void);
+};
+
+#endif // FREEDRAW_H
